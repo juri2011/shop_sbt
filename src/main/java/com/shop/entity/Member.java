1438c55b91c545b4d8bf3ad145f7cc1dde3fd815
오주리 <ojl98@naver.com>
2024-04-15 23:37:10 +0900

    Member 엔티티 생성 및 createMember 메소드 작성

---
+++ src/main/java/com/shop/entity/Member.java
@@ -0,0 +1,50 @@
+package com.shop.entity;
+
+import com.shop.constant.Role;
+import com.shop.dto.MemberFormDto;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@Builder
+public class Member {
+
+    @Id
+    @Column(name="member_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private String address;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    //회원가입 -> memberFormDto -> member
+    public static Member createMember(MemberFormDto memberFormDto,
+                                      PasswordEncoder passwordEncoder){
+        Member member = Member.builder()
+                .name(memberFormDto.getName())
+                .email(memberFormDto.getEmail())
+                .address(memberFormDto.getAddress())
+                .password(passwordEncoder.encode(memberFormDto.getPassword()))
+                .role(Role.ADMIN)
+                .build();
+
+        return member;
+    }
+
+}
